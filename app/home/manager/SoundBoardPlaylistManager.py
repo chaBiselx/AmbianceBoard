c5fcf172a54841eeb09926e9b347f10facb0a5ab
BISELX Charles <charles.biselx@softeam.fr>
2024-12-24 14:27:39 +0100

    🚑 fix manager organizer

--- app/home/manager/SoundBoardPlaylistManager.py
+++ app/home/manager/SoundBoardPlaylistManager.py
@@ -1,3 +1,4 @@
+import logging
 from ..models.SoundBoard import SoundBoard
 from ..models.Playlist import Playlist
 from ..service.PlaylistService import PlaylistService
@@ -8,9 +9,9 @@ class SoundBoardPlaylistManager:
         self.soundboard = soundboard
 
     def get_playlists(self):
-        return (PlaylistService(self.request)).get_all_playlist()
+        return list(self.soundboard.playlists.all())
 
     def get_unassociated_playlists(self):
-        all_playlists = self.get_playlists()
-        associated_playlists = self.soundboard.playlists.all()
+        all_playlists = list((PlaylistService(self.request)).get_all_playlist())
+        associated_playlists = list(self.soundboard.playlists.all())
         return [playlist for playlist in all_playlists if playlist not in associated_playlists]
