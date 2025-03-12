d2ef684f88a975bb05bd03ebf7b30ba4e364a95a
BISELX Charles <charles.biselx@softeam.fr>
2025-03-12 11:01:35 +0100

    🚑 fix regex

--- app/home/templatetags/FrontJs.py
+++ app/home/templatetags/FrontJs.py
@@ -12,7 +12,6 @@ def search_true_file(name_file):
     js_dir = os.path.join(static_root, 'js')
     files = os.listdir(js_dir)
 
-
-    js_files = [f for f in files if re.match(rf"{name_file}\.[a-f0-9]+\.js", f)]
+    js_files = [f for f in files if re.match(rf"{name_file}\.[a-zA-Z0-9]+\.js", f)]
     return 'js/' + js_files[0] if js_files else None
     
