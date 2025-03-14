65cbb781bb27efadda5192d84aa4e65e91372137
BISELX Charles <charles.biselx@softeam.fr>
2025-03-14 17:25:07 +0100

    🐛 fix bugs

--- app/home/service/utils/StaticFilesService.py
+++ app/home/service/utils/StaticFilesService.py
@@ -10,7 +10,6 @@ class StaticFilesService:
     
     def search(self, filename: str) -> str|None:
         files = os.listdir(self.folder)
-        print(files)
     
         js_files = [f for f in files if re.match(rf"{filename}\.[a-zA-Z0-9-_]+\.js", f)]
         return js_files[0] if js_files else None
--- app/home/templatetags/Register.py
+++ app/home/templatetags/Register.py
@@ -4,6 +4,4 @@ register = template.Library()
 
 @register.filter(name='has_group') 
 def has_group(user, group_name):
-    print(user)
-    print(group_name)
     return user.groups.filter(name=group_name).exists() 
