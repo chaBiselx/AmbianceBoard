0889a4a6e2a23a0451dbf9c72b2f1b1fe5f8db73
BISELX Charles <charles.biselx@softeam.fr>
2025-03-25 15:35:35 +0100

    🔒️ fix middleware log

--- app/home/middleware/LogRequestsMiddleware.py
+++ app/home/middleware/LogRequestsMiddleware.py
@@ -12,16 +12,17 @@ class LogRequestsMiddleware:
         start_time = time.time()
         
         if request.user.is_authenticated: # If the user is authenticated
-            unique_id = f"USER_{request.user.uuid}"
+            log_id = f"USER_{request.user.uuid}"
             response = self.get_response(request)
         else: # If the user is not authenticated
             unique_id = request.COOKIES.get('unique_id')
-            if not unique_id: # If the cookie is not set
-                unique_id = f"COOKIE_{uuid.uuid4()}"
+            if not unique_id or not isinstance(unique_id, uuid.UUID) : # If the cookie is not set or modified
+                unique_id = f"{uuid.uuid4()}"
                 response = self.get_response(request)
                 response.set_cookie('unique_id', unique_id, max_age=3600)  # Set cookie for 1 hour
             else:
                 response = self.get_response(request)
+            log_id = f"ANONYMOUS_{unique_id}"
             
         duration = round(time.time() - start_time, 6)
 
@@ -43,7 +44,7 @@ class LogRequestsMiddleware:
                 'unique_id': unique_id
             }
         )
-
+ 
         self.logger_home.info(
             f"REQUEST : {request.method:<8} {request.get_full_path()} {filtered_post} {response.status_code} {duration}sec id:{unique_id}"
         )
