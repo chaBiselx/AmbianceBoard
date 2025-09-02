42bdf5e6c330567108c3df5f4e2aad01b555b7d7
BISELX Charles <cha.biselx@gmail.com>
2025-09-02 08:04:41 +0200

    fix error

--- app/main/domain/common/repository/UserPreferenceRepository.py
+++ app/main/domain/common/repository/UserPreferenceRepository.py
@@ -3,6 +3,8 @@ from main.architecture.persistence.models.UserPreference import UserPreference
 
 class UserPreferenceRepository:
 
-    def get_user_preferences(self, user: User) -> UserPreference:
-        return UserPreference.objects.get(user=user)
-        
+    def get_user_preferences(self, user: User) -> UserPreference | None:
+        try:
+            return UserPreference.objects.get(user=user)
+        except UserPreference.DoesNotExist:
+            return None
