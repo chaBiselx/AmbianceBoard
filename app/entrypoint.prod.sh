bd078938746a66490c206d5f87d85678cccd585a
BISELX Charles <cha.biselx@gmail.com>
2025-09-02 14:19:11 +0200

    test

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -50,9 +50,9 @@ if [ "$RUNCRON" = "1" ]; then
     exec cron -f
 fi
 
-if [ "$RUNCRON" != "1" ]; then
+if [ "$DAPHNE" == "1" ]; then
     echo "Start WebSocket Daphne..."
-    exec daphne parameters.asgi:application --bind 0.0.0.0 --port ${WS_PORT:-8000} &
+    exec daphne parameters.asgi:application --bind 0.0.0.0 --port ${WS_PORT:-8081} &
 fi
 
 
