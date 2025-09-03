6a9e62d5c9a74e38767bcac05b6b77dad83af59c
BISELX Charles <cha.biselx@gmail.com>
2025-09-03 15:04:47 +0200

    fix

--- app/main/domain/common/repository/UserRepository.py
+++ app/main/domain/common/repository/UserRepository.py
@@ -3,11 +3,23 @@ from main.architecture.persistence.models.User import User
 
 
 class UserRepository:
-
-    def get_user_by_email(self, email: str) -> Optional[User]:
-        return User.objects.filter(email=email).first()
     
-    def get_user_by_username(self, username: str) -> Optional[User]:
-        return User.objects.filter(username=username).first()
+    def get_user(self, uuid_user: str) -> User | None:
+        try:
+            return User.objects.get(uuid=uuid_user)
+        except User.DoesNotExist:
+            return None
 
+
+    def get_user_by_email(self, email: str) -> User | None:
+        try:
+            return User.objects.filter(email=email).first()
+        except User.DoesNotExist:
+            return None
+    
+    def get_user_by_username(self, username: str) -> User | None:
+        try:
+            return User.objects.filter(username=username).first()
+        except User.DoesNotExist:
+            return None
 
--- app/main/domain/general/views/confirmViews.py
+++ app/main/domain/general/views/confirmViews.py
@@ -3,22 +3,26 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 from main.architecture.persistence.models.User import User
+from main.domain.common.repository.UserRepository import UserRepository
 from main.domain.common.utils.logger import logger
 from main.domain.common.service.ConfirmationUserService import ConfirmationUserService
 from main.domain.common.enum.HtmlDefaultPageEnum import HtmlDefaultPageEnum
+from main.domain.common.utils.ServerNotificationBuilder import ServerNotificationBuilder
 
 
 @require_http_methods(['GET'])
 def confirm_account(request, uuid_user:str, confirmation_token:str):
     logger.info("Starting ConfirmAccount View")
-    try:
-        user = User.objects.get(uuid=uuid_user)
-        ConfirmationUserService(user).verification_token(confirmation_token)
-        
-       
-
+    user = UserRepository().get_user(uuid_user)
+    if user:
+        confirmed = ConfirmationUserService(user).verification_token(confirmation_token)
+        if confirmed:
+            ServerNotificationBuilder(request).set_message(
+                "Votre compte a été validé."
+                ).set_statut("info").send()
+            
         return redirect('login')
-    except Exception as e:
-        logger.error(e)
+    else:
+        logger.error("confirm_account User not found")
         return render(request,  HtmlDefaultPageEnum.ERROR_404.value, status=404)
     
