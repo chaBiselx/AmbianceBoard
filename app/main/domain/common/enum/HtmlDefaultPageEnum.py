957aa248028e2d9b049d150247c7f44493a62af0
charles biselx <cha.biselx@gmail.com>
2025-12-03 08:58:50 +0100

    Ajout page erreur 40X modal

--- app/main/domain/common/enum/HtmlDefaultPageEnum.py
+++ app/main/domain/common/enum/HtmlDefaultPageEnum.py
@@ -3,5 +3,7 @@ from .BaseEnum import BaseEnum
 class HtmlDefaultPageEnum(BaseEnum): 
     HOME = 'Html/home/home.html'
     ERROR_404 = 'Html/General/404.html'
+    ERROR_404_MODAL = 'Html/General/404_modal.html'
     ERROR_403 = 'Html/General/403.html'
+    ERROR_403_MODAL = 'Html/General/403_modal.html'
     ERROR_429 = 'Html/General/429.html'
