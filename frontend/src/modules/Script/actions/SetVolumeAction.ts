0dd8ee6ee7fb744374338ced695aad279bac2747
charles BISELX <cha.biselx@gmail.com>
2026-09-07 21:16:11 +0200

    fix issue sonar

--- frontend/src/modules/Script/actions/SetVolumeAction.ts
+++ frontend/src/modules/Script/actions/SetVolumeAction.ts
@@ -6,7 +6,8 @@ import { ScriptStepDTO } from '@/modules/Script/ScriptTypes';
 
 class SetVolumeAction implements IScriptAction {
     execute(step: ScriptStepDTO): ScriptStepHandle {
-        const playlistId = String(step.params.playlist_uuid ?? '');
+        const playlistUuid = step.params.playlist_uuid;
+        const playlistId = typeof playlistUuid === 'string' ? playlistUuid : '';
         const buttonPlaylist = ButtonPlaylistFinder.search(playlistId);
         if (buttonPlaylist) {
             const volume = Number(step.params.volume ?? 100);
